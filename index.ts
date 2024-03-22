8072cf6742dfe9e1f9c9d15913e3d8d1d57b0042
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-03-23 03:33:09 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+//computer will generate a random number
+//user input for guessing number
+//compare to computer generate random number with user number and show result
+const randomNumber = Math.floor(Math.random()*6+1);
+const answer = await inquirer.prompt([{
+    name : "userGuessNumber",
+    type : "number",
+    message : "Please guess a number between 1-6 :",
+},
+]);
+console.log(answer);
+if(answer.userGuessNumber === randomNumber){
+    console.log("Congratulations! you win");
+}else{
+    console.log("Oops! you failed");
+}
